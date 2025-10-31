85fdb32ce9ff3c6b48bc28e96f10185ee60dcc8a
Harshit Jaiswal <harshitjaiswal022@gmail.com>
2025-10-31 19:26:01 +0530

    worked on Navbar

--- Project-4/app/src/App.jsx
+++ Project-4/app/src/App.jsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <MainContainer>
+      <TopContainer>
+        <div className="logo">
+          <img src="/Image/logo.png" alt="logo" />
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search Food" />
+        </div>
+      </TopContainer>
+    </MainContainer>
+  );
 };
 
 export default App;
+
+const MainContainer = styled.div``;
+const TopContainer = styled.section``;
